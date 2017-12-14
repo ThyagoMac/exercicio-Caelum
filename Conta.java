47ab79f99a46d7d57edf3bc14a1942b7f1300567
Thyago Mac Cormick <32471417+ThyagoMac@users.noreply.github.com>
2017-12-14 21:19:06 -0200

    Add files via upload

--- Conta.java
+++ Conta.java
@@ -1,46 +1,85 @@
 class Conta{
-	String titular;
-	int numero;
-	String agencia;
-	double saldo;
-	Data data;
+	public static int contador;
+	private String titular;
+	private int numero;
+	private String agencia;
+	private double saldo;
+	private Data data;
+	//geters
+	public String getTitular(){
+		return this.titular;
+	}
+	public int getNumero(){
+		return this.numero;
+	}
+	public String getAgencia(){
+		return this.agencia;
+	}
+	public double getSaldo(){
+		return this.saldo;
+	}
+	public Data getData(){
+		return this.data;
+	}
+	//seters
+	public void setTitular(String titular){
+		this.titular = titular;
+	}
+	public void setNumero(int numero){
+		this.numero = numero;
+	}
+	public void setAgencia(String agencia){
+		this.agencia = agencia;
+	}
+	public void setSaldo(double saldo){
+		this.saldo = saldo;
+	}
+	public void setData(Data data){
+		this.data = data;
+	}
+
+	//constructor
+	public Conta() {
+		contador++;
+System.out.println("Novo obj Conta criado numero total de Contas: " + contador);
+	}
 
 	void saca(double valor){
-		if (valor <= this.saldo){
+		if (valor <= this.getSaldo()){
 
 			System.out.println("\n---Operação de Saque---"); 
-			System.out.println("Valor antes da operação: " + this.saldo);
-			this.saldo -= valor;
+			System.out.println("Valor antes da operação: " + this.getSaldo());
+			this.setSaldo(this.getSaldo() + valor);
 			System.out.println("Sacado: " + valor);
-			System.out.println("Saldo atual: " + this.saldo);
+			System.out.println("Saldo atual: " + this.getSaldo());
 			System.out.println("----------\n");
 		} else {
 			System.out.println("Não foi possivel realisar sua operação!!! Saldo insuficiente!!!\n" +
-					"Saldo: "+ this.saldo + " Valor digitado: " + valor);
+					"Saldo: "+ this.getSaldo() + " Valor digitado: " + valor);
 		}
 	}
 	void deposita(double valor){
 		
 		System.out.println("\n---Operação de Deposito---");
-		System.out.println("Valor antes da operação: " + this.saldo);
-		this.saldo += valor;
+		System.out.println("Valor antes da operação: " + this.getSaldo());
+		this.setSaldo(this.getSaldo() + valor);
 		System.out.println("Depositado: " + valor);
-		System.out.println("Saldo atual: " + this.saldo);
+		System.out.println("Saldo atual: " + this.getSaldo());
 		System.out.println("----------\n");
 		
 	}
 
 	double calculaRendimento(){
-		double rendimento = this.saldo * 0.01;
+		double rendimento = this.getSaldo() * 0.01;
 		return rendimento;
 	}
 
 	String recuperaDadosParaImpressao(){
 		String dados = "\n---Dados da conta---\n" +
-				"Titular: " + this.titular +
-				"\n Numero: " + this.numero +
-				"\n Agencia: " + this.agencia +
-				"\n Saldo: " + this.saldo +
+				"Titular: " + this.getTitular() +
+				"\n Numero: " + this.getNumero() +
+				"\n Agencia: " + this.getAgencia() +
+				"\n Saldo: " + this.getSaldo() +
 				"\n Data de Abertura: " + this.data.dataCompleta() +
 				"\n Rendimento Mensal: " + this.calculaRendimento() +
 				"\n-----------------------\n";
